48fe3075c0dd2edb3eae30799c30691569643d8f
aaronmgodfrey <aaronmgodfrey@outlook.com>
2025-12-09 10:13:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const commandPrefix = '!g ';
 const greetings = ['Hi', 'Hello', 'Hey', 'Hey there', 'Hi', 'Heya', 'Sup', 'Hoi', 'Hia'], emoticons = ['^V^', ':)', ':]', ':D', '=)', '=]', '=D'];
 client.on('messageCreate', async message => {
   const lower = message.content.toLowerCase();
-  if ((lower.includes('greg') || message.content.includes('<@1447959380787200021>')) {
+  if (lower.includes('greg') || message.content.includes('<@1447959380787200021>')) {
     if (greetings.some(g => lower.includes(g.toLowerCase()))) message.reply(greetings[Math.floor(Math.random()*greetings.length)]+' '+emoticons[Math.floor(Math.random()*emoticons.length)]);
     if (lower.includes('r3')) message.reply('<@783362675761348629>');
   }
